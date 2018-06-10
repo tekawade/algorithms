89e822d54fca30b9d99849fe2585a057dbd2e899
tekawade <tekawade@rahuls-mbp>
2018-06-10 01:43:37 -0700

    First GitHub Commit

---
+++ algorithms/src/algorithms/HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package algorithms;
+
+/**
+ * @author tekawade
+ *
+ */
+public class HelloWorld {
+
+	private static int helloWorldMsg (String[] args)
+	{
+		int err = 0;
+		
+		System.out.println("Hello, World!");
+		
+		return err;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int status = helloWorldMsg(args);
+		System.exit(status);
+	}
+
+}
